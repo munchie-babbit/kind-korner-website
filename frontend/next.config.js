fd15cdc275f2c5d1bc179c37e6fbc7fb37a31b7c
Hoang Phan <phan.minhhoang2606@gmail.com>
2023-10-28 23:33:07 -0400

    Fix frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 // Remove output: "export" from {} to run locally
-const nextConfig = {
-  images: {
-    domains: ["via.placeholder.com"],
-  },
-};
+const nextConfig = { output: "export" };
 
 module.exports = nextConfig;
