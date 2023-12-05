ff53736078ffbe3b688660514026257fd41e4912
munchie-babbit <estellechung333@gmail.com>
2023-12-05 15:08:36 -0500

    added preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta property="og:image" content="/images/preview.jpg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
